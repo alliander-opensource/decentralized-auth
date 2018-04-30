312890a668df33cbeb9ca790ef0677ae4a510736
Erwin Rooijakkers <erwin.rooijakkers@alliander.com>
2018-04-30 19:48:31 +0200

    chore: test net as default provider

--- raspberry-pi-client/src/config.js
+++ raspberry-pi-client/src/config.js
@@ -3,7 +3,7 @@ const config = {
   smartMeterVersion: 2.2, // DSRM 2.2, 4.0, 4.2, or 5.0
   p1SerialPort: process.env.P1_SERIAL_PORT ? process.env.P1_SERIAL_PORT : '/dev/ttyUSB0',
 
-  iotaProvider: process.env.IOTA_PROVIDER ? process.env.IOTA_PROVIDER : 'http://localhost:14700',
+  iotaProvider: process.env.IOTA_PROVIDER ? process.env.IOTA_PROVIDER : 'http://node01.testnet.iotatoken.nl:16265',
   iotaMinWeightMagnitude: 10, // Only works on testnet, use >14 for main net
 
   seed: process.env.SEED ? process.env.SEED : 'TLQPEYBND9AFCHFDLCWSVQU9ISCDTBKUQSLXEEUXFHVDEEQZZJPCBPBJ9QSVFBXUJXTIFBMTQSLVUFYTH',
