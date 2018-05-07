cf16fe310ced5830084f9294fba5d01b74381189
Erwin Rooijakkers <erwin.rooijakkers@alliander.com>
2018-05-07 20:40:25 +0200

    refactor: rename dc deviceClient

--- raspberry-pi-client/src/main.js
+++ raspberry-pi-client/src/main.js
@@ -3,11 +3,10 @@ const config = require('./config');
 const logger = require('./logger')(module);
 const DeviceClient = require('./device-client');
 
-const dc = new DeviceClient(
+const deviceClient = new DeviceClient(
   config.seed,
   config.secret,
   config.initialSideKey,
 );
-logger.info(`Started a device client ${util.inspect(dc)} !`);
 
-module.exports = { dc };
+logger.info(`Started a device client ${util.inspect(deviceClient)} !`);
