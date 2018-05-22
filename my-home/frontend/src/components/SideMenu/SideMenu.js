a0c94116f09107f4ac0e86386f20c485627dfd8f
Erwin Rooijakkers <erwin.rooijakkers@alliander.com>
2018-05-22 19:03:17 +0200

    feat: use different icons in side menu

--- my-home/frontend/src/components/SideMenu/SideMenu.js
+++ my-home/frontend/src/components/SideMenu/SideMenu.js
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom';
 import Paper from 'material-ui/Paper';
 import { List, ListItem } from 'material-ui/List';
 import Divider from 'material-ui/Divider';
-import IconActionHome from 'material-ui/svg-icons/action/home';
+import IconSocialMood from 'material-ui/svg-icons/social/mood';
+import IconHardwareDeviceHub from 'material-ui/svg-icons/hardware/cast';
 
 const SideMenu = () => {
   const style = {
@@ -16,10 +17,10 @@ const SideMenu = () => {
         <Divider />
         <List>
           <Link to="/my-devices">
-            <ListItem primaryText="My Devices" leftIcon={<IconActionHome />} />
+            <ListItem primaryText="My Devices" leftIcon={<IconHardwareDeviceHub />} />
           </Link>
           <Link to="/my-policies">
-            <ListItem primaryText="My Policies" leftIcon={<IconActionHome />} />
+            <ListItem primaryText="My Policies" leftIcon={<IconSocialMood />} />
           </Link>
         </List>
       </Paper>
