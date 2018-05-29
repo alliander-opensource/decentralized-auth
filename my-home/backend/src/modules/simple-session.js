4a8e5ebeec151d0840d1dde8bbcb5b5e16e5b8c0
Erwin Rooijakkers <erwin.rooijakkers@alliander.com>
2018-05-29 16:47:12 +0200

    refactor: extract methods for session state init

--- my-home/backend/src/modules/simple-session.js
+++ my-home/backend/src/modules/simple-session.js
@@ -13,28 +13,15 @@ const sessionState = require('./../session-state');
 const logger = require('./../logger')(module);
 const generateSeed = require('./../modules/gen-seed');
 
-// Initializes loads of stuff. TODO: refactor (extract methods); add docstring
-async function deauthenticate(req, res) {
-  const sessionId = uuidv4();
 
-  req.sessionId = sessionId;
-  res.cookie(config.cookieName, req.sessionId, config.cookieSettings);
+const addIotaAddress = async (seed, sessionId) => {
+  const [address] = await iota.getAddress(seed, 1);
+  logger.info(`Storing IOTA address ${address} for session ${sessionId}`);
+  sessionState[sessionId].iotaAddress = address;
+};
 
-  const seed = await generateSeed();
 
-  sessionState[sessionId] = {};
-
-  logger.info(`Storing new IOTA seed ${seed} for session ${sessionId}`);
-  sessionState[sessionId].iotaSeed = seed;
-
-  const setIotaAddress = async () => {
-    const [address] = await iota.getAddress(seed, 1);
-    logger.info(`Storing IOTA address ${address} for session ${sessionId}`);
-    sessionState[sessionId].iotaAddress = address;
-  };
-
-  await setIotaAddress();
-
+const addMamState = (seed, sessionId) => {
   // Our MAM channel for publishing information to for the device
   // We also use it as a database (to read the event stream and build the state
   // using `projections.js`)
@@ -49,7 +36,34 @@ async function deauthenticate(req, res) {
   const mamRoot = mam.getMamState().channel.next_root;
   logger.info(`Set MAM root to ${mamRoot} for session ${sessionId}`);
   sessionState[sessionId].mamRoot = mamRoot;
+};
+
+
+/**
+ * (Re-)initializes the session. Creates a new session id and the instances used
+ * in the session.
+ *
+ * @function deauthenticate
+ * @param {object} req Express request object
+ * @param {object} res Express response object
+ */
+async function deauthenticate(req, res) {
+  const sessionId = uuidv4();
+
+  req.sessionId = sessionId;
+  res.cookie(config.cookieName, req.sessionId, config.cookieSettings);
+
+  const seed = await generateSeed();
+
+  sessionState[sessionId] = {};
+
+  logger.info(`Storing new IOTA seed ${seed} for session ${sessionId}`);
+  sessionState[sessionId].iotaSeed = seed;
+
+  await addIotaAddress(seed, sessionId);
+  addMamState(seed, sessionId);
 }
+
 
 function simpleSessionCookieParser(req, res, next) {
   if (!req.signedCookies[config.cookieName]) {
@@ -60,6 +74,7 @@ function simpleSessionCookieParser(req, res, next) {
   }
   next();
 }
+
 
 module.exports = simpleSessionCookieParser;
 module.exports.deauthenticate = deauthenticate;
