3a85099b8c8bb4dbafedcf71ec6e5c77c0646e2a
Erwin Rooijakkers <erwin.rooijakkers@alliander.com>
2018-05-30 15:47:16 +0200

    chore: remove unused method and dependency

--- my-home/backend/src/common/test-utils.js
+++ my-home/backend/src/common/test-utils.js
@@ -14,33 +14,10 @@ const { expect } = chai;
 const { app } = require('../server');
 const { server } = require('../server');
 
-const getRandomValues = require('get-random-values');
-
-
-// Insecure way to generate an IOTA seed
-// SOURCE: https://www.reddit.com/r/Iota/comments/7sc62r/seed_generator_in_javascript/
-const generateSeedForTestingPurposes = () => {
-  const length = 81;
-  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ9';
-  const randomValues = new Uint8Array(length);
-  const result = new Array(length);
-
-  getRandomValues(randomValues);
-
-  let cursor = 0;
-  for (let i = 0; i < randomValues.length; i += 1) {
-    cursor += randomValues[i];
-    result[i] = chars[cursor % chars.length];
-  }
-
-  return result.join('');
-};
 
 module.exports = {
   app,
   server,
   expect,
   request,
-
-  generateSeedForTestingPurposes,
 };
