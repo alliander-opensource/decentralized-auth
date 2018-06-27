15cef955d4e580c91d48c4528d0272c1d8f9eaa0
Erwin Rooijakkers <erwin.rooijakkers@alliander.com>
2018-06-27 22:30:48 +0200

    fix: spelling mistake and typo

--- my-home/frontend/src/containers/Home/Home.js
+++ my-home/frontend/src/containers/Home/Home.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Home = () => (
   <div style={{ padding: '20px' }}>
     <h2>My Home</h2>
-    This application works with one IOTA seed. It&apos;s backend is a public MAM
-    stream on this seed.
+    This application works with one IOTA seed. Its backend is a private MAM
+    channel from this seed.
     <br />
     <br />
     Pair with devices, give consent to service providers to view their data, or
